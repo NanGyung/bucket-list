26e55948edd0e5946abd1c74852352856de43e48
“NanGyung” <rhrnakaktdidrod0926@gmail.com>
2023-03-14 14:00:39 +0900

    react-native 능력단위평가

---
+++ bucket-list/src/theme.js
@@ -0,0 +1,11 @@
+export const theme = {
+    background: '#F7FAFC',
+    titleBackground: '#3C56B1',
+    itemBackground: '#BBD5E4',
+    inputBackground: '#1E475F',
+    inputText: '#F5F5F5',
+    main: '#CAE6F5',
+    text: '#1B1B1B',
+    titleText: '#fefefe',
+    done: '#767C80'
+};
